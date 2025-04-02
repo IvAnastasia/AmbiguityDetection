fe11d57042b7790059bfa92195e57c530f100dbd
IvAnastasia <71773596+IvAnastasia@users.noreply.github.com>
2025-04-02 11:15:01 +0200

    Add files via upload

---
+++ affordances/prompts.py
@@ -0,0 +1,3 @@
+examples_generation = "\n".join(open("affordances/prompts/generation.txt", encoding="utf-8").readlines())
+affordance_generation = "\n".join(open("affordances/prompts/affordances.txt", encoding="utf-8").readlines())
+answer_generation ="\n".join(open("affordances/prompts/choising.txt", encoding="utf-8").readlines())
